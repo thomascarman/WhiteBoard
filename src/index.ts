a0ea4bdeb166331ed5b3155bf0a1105a1d8a6c80
Thomas Carman <thomascarman@users.noreply.github.com>
2019-09-05 12:35:51 -0700

    Setup Input Form

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ class OpsCheckEntry {
 class OpsCheck {
   entries: Array<OpsCheckEntry> = [];
 
-  addEntry(values: Array<number>) {
+  addEntry(values: Array<number> = [0, 0, 0, 0, 0]) {
     const objInput = {
       time: new Date(),
       oil: {
@@ -54,9 +54,37 @@ class OpsCheck {
         level: values[4]
       }
     };
+
     this.entries.push(new OpsCheckEntry(objInput));
   }
 }
 
 let opCheck = new OpsCheck();
-opCheck.addEntry([1, 2, 3, 4, 5]);
+
+const app: HTMLElement = document.getElementById("app");
+app.innerHTML = `<h1>Digital White Board</h1>
+  <div class="formInputs">
+    <ul>
+      <li>
+        Oil
+        <ul>
+          <li><div>Level: </div><input type="text" value="" /></li>
+          <li><div>Temp: </div><input type="text" value="" /></li>
+          <li><div>Pres: </div><input type="text" value="" /></li>
+        </ul>
+      </li>
+      <li>
+        Fuel
+        <ul>
+          <li><div>Rem: </div><input type="text" value="" /></li>
+          <li><div>Level: </div><input type="text" value="" /></li>
+        </ul>
+      </li>
+    </ul>
+    <input id="submit" type="submit" />
+  </div>
+  <div class="outputForm"></div>
+`;
+
+const submitBtn: HTMLElement = document.getElementById("submit");
+submitBtn.addEventListener("click", e => opCheck.addEntry());
